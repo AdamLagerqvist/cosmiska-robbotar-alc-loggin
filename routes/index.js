60c6a65111bddda94f051968a8101f309095c782
AdamLagerqvist <adam.lagerqvist@elev.ga.ntig.se>
2022-03-25 10:53:19 +0100

    🩹 skellet

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.njk', { title: 'Homepage' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login.njk', { title: 'login' });
+});
+router.get('/secret', function(req, res, next) {
+  res.render('secret.njk', { title: 'secret' });
+});
+router.get('/register', function(req, res, next) {
+  res.render('register.njk', { title: 'register' });
 });
 
 module.exports = router;
