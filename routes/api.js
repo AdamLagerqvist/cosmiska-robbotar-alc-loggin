41ce7798a2d12eb5e25c2b846051eba78c48582f
AdamLagerqvist <adam.lagerqvist@elev.ga.ntig.se>
2022-04-01 11:17:02 +0200

    pass 2 fredag 04-01

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,26 @@ var bcrypt = require('bcrypt');
 const pool = require('../database');
 var router = express.Router();
 
+router.post('/register', async function (req, res, next) {
+    const username = req.body.user;
+    const password = req.body.password;
+    if(password != req.body.password2){
+        res.status(500).redirect("/login?response=2");
+    }
+    bcrypt.hash(password, 10, async function (err, hash) {
+
+        await pool.promise()
+        .query('INSERT INTO login (user, password) VALUES (?,?)', [username,hash])
+        .then(([rows, fields]) => {
+            req.session.user = username;
+            res.redirect("/secret");
+        }).catch(err => {
+            console.log(err)
+            res.status(500).redirect("/register?response=1");
+        });
+
+    });
+})
 /* GET users listing. */
 router.post('/login', async function (req, res, next) {
 
@@ -25,7 +45,7 @@ router.post('/login', async function (req, res, next) {
                         
                         return res.redirect("/secret");
                     } else {
-                        res.status(500).redirect("/login?response=1");
+                        res.status(500).redirect("/login?response=0");
                     }
 
                 });
@@ -34,7 +54,7 @@ router.post('/login', async function (req, res, next) {
 
         })
         .catch(err => {
-            res.status(500).redirect("/login?response=1");
+            res.status(500).redirect("/login?response=0");
         });
 });
 
@@ -44,6 +64,11 @@ router.get('/test', function (req, res, next) {
         res.send(hash);
         console.log(hash)
     });
+});
+
+router.post('/logout', function (req, res, next) {
+    req.session.user = null;
+    res.redirect("/login");
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,26 @@
 var express = require('express');
+const { response } = require('../app');
 var router = express.Router();
+var responses = [
+"Wrong username or password.",
+"Username already exists",
+"Passwords do not match",
+"lol easter egg"
+]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.njk', { title: 'Homepage' });
+  res.render('index.njk', { title: 'Homepage', user: req.session.user });
 });
 router.get('/login', function(req, res, next) {
-  res.render('login.njk', { title: 'login' });
+  res.render('login.njk', { title: 'login', response: responses[req.query.response]||null, user: req.session.user });
 });
 router.get('/secret', function(req, res, next) {
+  if(!req.session.user) return res.redirect("/login");
   res.render('secret.njk', { title: 'secret', user: req.session.user });
 });
 router.get('/register', function(req, res, next) {
-  res.render('register.njk', { title: 'register' });
+  res.render('register.njk', { title: 'register', response: responses[req.query.response]||null, user: req.session.user });
 });
 
 module.exports = router;
